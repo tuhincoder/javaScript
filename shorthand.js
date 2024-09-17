ed18bd62567719ce403dcafa24acb1b1c3428d64
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-17 13:31:18 +0600

    js practice

---
+++ shorthand.js
@@ -0,0 +1,39 @@
+
+// var potatoPrice = 30;
+// potatoPrice = 50;
+
+// var price1 = 20;
+// var price2 = 30;
+// price1 =
+//     console.log(price1);
+
+// --------
+var onionPrice = 20;
+var eggPrice = 10;
+
+var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+var priceDifference = onionPrice - eggPrice;
+// console.log(priceDifference);
+
+//---------------multiplication
+var orangePrice = 20;
+var quantity = 7;
+var totalCoast = orangePrice * quantity;
+// console.log('total coast', totalCoast);
+
+var mobilePrice = 1000;
+var quantity = 2;
+var totalCost = mobilePrice * quantity;
+// console.log(totalCoast);
+// console.log(mobilePrice * quantity);
+
+// -----------------
+
+
+
+
+
+
+
+
